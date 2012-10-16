c9a0ee50c2e252ddef0d30e311df974cf605ecdc
Klaus Kämpf <kkaempf@suse.de>
2012-10-16 10:13:39 +0200

    Pass debuglevel on command line

--- lib/usbmon/digitdia.rb
+++ lib/usbmon/digitdia.rb
@@ -9,25 +9,40 @@
 
 module UsbMon
   class DigitDia
-    def initialize
+    def initialize debug_level = 0
+      @debug_level = debug_level
       @state = :idle
       @counts = {}
       @ieee1284_cmd_index = 0
       # cmd at index 7
       @ieee1284_cmd_sequence = [0xff, 0xaa, 0x55, 0x00, 0xff, 0x87, 0x78, 0x00, 0xff]
       @scsi_cmd_index = 0
+      @scsi_cmd_data = []
       @scsi_extra_count = 0
+      @scsi_extra_data = []
     end
     
-    def message msg, event
+    #
+    # type: :usb (lowest level)
+    #       :ctrl (ieee1284)
+    #       :cmd (scsi cmd prep)
+    #       :scsi
+    #       :top
+    #
+    DEBUG_LEVELS = [ :none, :top, :scsi, :cmd, :ctrl, :usb ]
+    def message type, msg, event
+      level = DEBUG_LEVELS.find_index type
+      raise "Unknown debug level #{type.inspect}" unless level
+#      puts "Message type #{type.inspect} (level #{level}), debug_level #{@debug_level}"
+      return if level > (@debug_level % 10)
       time = (event.timestamp - @start) / 1000
       if @previous
         delta = time - @previous
       else
         delta = 0
       end
-      puts "(+%5d ms) t+%5d: %s" % [delta.to_i, time.to_i, msg]
-      puts "\t\t#{event.raw}"
+      puts "(+%5d ms) t+%5d: #{type} - %s" % [delta.to_i, time.to_i, msg]
+      puts "\t\t#{event.raw}" if @debug_level > 10
       @previous = time
     end
     #
@@ -36,6 +51,59 @@ module UsbMon
     def increment what, data
       @counts[what] ||= []
       @counts[what] << data
+    end
+
+    def get_value data
+      low = data.shift.hex
+      low + data.shift.hex*256
+    end
+
+    def explain_filter val
+      case val
+      when 1
+        "filter neutral"
+      when 2
+        "filter red"
+      when 4
+        "filter green"
+      when 8
+        "filter blue"
+      else
+        "filter #{val}"
+      end
+    end
+    #
+    # explain 'write' sub-commands
+    # see "command codes used in the data part of a SCSI write command" at end of backed/pie-scsidef.h
+    #    
+    def explain_write data, event
+      cmd = get_value data
+      len = get_value data
+      if data.size < len # consistency check
+        raise "Bad len (#{len}), only have #{data.size} bytes for #{data.inspect}"
+      end
+      case cmd
+      when 0x12
+        msg = "set_scan_frame"
+      when 0x13
+        fil = explain_filter(get_value data)
+        val = get_value data
+        msg = "set_exp_time #{fil}:#{val}%"
+      when 0x14
+        fil = explain_filter(get_value data)
+        val = get_value data
+        msg = "set_highlight_shadow #{fil}:#{val}%"
+      when 0x95
+        msg = "read_cal_info"
+      else
+        msg = "???<0x%02x>" % cmd
+      end
+      while !data.empty?
+        val = get_value data
+        msg << (" (%d/0x%04x)" % [val,val])
+#        cmd << " #{val.inspect}"
+      end
+      message :scsi, "\t#{msg} #{data.inspect}", event
     end
 
     #################################
@@ -69,7 +137,7 @@ module UsbMon
         raise "Bad state #{@state}"
       end
       @state = :await_callback
-      message ("\tGet status %04x,%04x" % [event.wValue,event.wIndex]), event
+      message :usb, ("\tGet status %04x,%04x" % [event.wValue,event.wIndex]), event
     end
     
     #
@@ -83,11 +151,11 @@ module UsbMon
         @state = event.utd
 	case event.bmRequestType
 	when 0x00 # Init
-	  message "usb init bmRequestType 0x00", event
+	  message :usb, "usb init bmRequestType 0x00", event
 	when 0x02 # Init
-	  message "usb 02 bmRequestType 0x02", event
+	  message :usb, "usb 02 bmRequestType 0x02", event
 	when 0x23 # 
-	  message "usb 23 bmRequestType 0x23", event
+	  message :usb, "usb 23 bmRequestType 0x23", event
 	when 0x40 # Vendor -> Device
           unless event.wValue == 0x0082
             unless event.wLength == 1
@@ -116,21 +184,19 @@ module UsbMon
                   elsif @scsi_extra_data[0] == '40'
                     direction = "new box"
                   else
-                    direction = "\n\tUNKNOWN DIRECTION\n"
+                    direction = "<UNKNOWN:#{@scsi_extra_data[0]}>" 
                   end
-                  message "SCSI #{direction} slide : #{@scsi_extra_data.inspect}", event
+                  message :scsi, ("\t #{direction} - 0x%02x:%s" % [@scsi_extra_data.size, @scsi_extra_data.inspect]), event
                 when "0a"
-                  message "SCSI write : #{@scsi_extra_data.inspect}", event
+                  explain_write @scsi_extra_data, event
+                when "15", "dc"
+                  message :scsi, ("\t - data - 0x%02x:%s" % [@scsi_extra_data.size, @scsi_extra_data.inspect]), event
                 when "10"
-                  message "SCSI mark : #{@scsi_extra_data.inspect}", event
+                  message :scsi, "mark : #{@scsi_extra_data.inspect}", event
                 when "11"
-                  message "SCSI space : #{@scsi_extra_data.inspect}", event
-                when "15"
-                  message "SCSI mode select : #{@scsi_extra_data.inspect}", event
-                when "dc"
-                  message "SCSI dc : #{@scsi_extra_data.inspect}", event
+                  message :scsi, "space : #{@scsi_extra_data.inspect}", event
                 else
-                  message "\t\t\t\t SCSI #{@scsi_cmd_current} : #{@scsi_extra_data.inspect}", event
+                  message :scsi, "**** #{@scsi_cmd_current} : #{@scsi_extra_data.inspect}", event
                 end
                 @scsi_cmd_index = 0
                 @scsi_extra_data = []
@@ -138,6 +204,7 @@ module UsbMon
               return
             end
             # SCSI, see pie.c
+            @scsi_cmd_data[@scsi_cmd_index] = event.data
             case @scsi_cmd_index
             when 0 then @scsi_cmd_current = event.data
             when 4 then @scsi_cmd_length = event.data
@@ -145,28 +212,48 @@ module UsbMon
               case @scsi_cmd_current
               when "00"
                 raise "Bad SCSI: #{event.raw}" unless @scsi_cmd_length == "00"                
-                message "SCSI reset (#{@scsi_cmd_length} bytes)", event
-              when "01" then message "SCSI calibrate (#{@scsi_cmd_length} bytes)", event
-              when "03" then message "SCSI sense (#{@scsi_cmd_length} bytes)", event
-              when "04" then message "SCSI format (#{@scsi_cmd_length} bytes)", event
-              when "08" then message "SCSI read (#{@scsi_cmd_length} bytes)", event
-              when "0a", "10", "11", "15", "d1", "dc"
+                message :scsi, "test_ready (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+              when "01" then message :scsi, "calibrate (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+              when "03" then message :scsi, "sense (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+              when "04" then message :scsi, "format (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+              when "08" then message :scsi, "read (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+              when "0a"
+                message :scsi, "write (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
                 @scsi_extra_count = @scsi_cmd_length.hex.to_i
                 @scsi_extra_data = []
-              when "0c" then message "SCSI 0c (#{@scsi_cmd_length} bytes)", event
-              when "0f" then message "SCSI 0f (#{@scsi_cmd_length} bytes)", event
-              when "12" then message "SCSI inquiry (#{@scsi_cmd_length} bytes)", event
-              when "18" then message "SCSI 18 (#{@scsi_cmd_length} bytes)", event
-              when "1a" then message "SCSI mode sense (#{@scsi_cmd_length} bytes)", event
-              when "1b" then message "SCSI load/unload : #{@scsi_cmd_length} bytes", event
-              when "1d" then message "SCSI diag (#{@scsi_cmd_length} bytes)", event
-              when "a8" then message "SCSI read (#{@scsi_cmd_length} bytes)", event
-              when "d7" then message "SCSI d7 (#{@scsi_cmd_length} bytes)", event
-              when "dd" then message "SCSI dd (#{@scsi_cmd_length} bytes)", event
-              when "ff" then message "SCSI ff (#{@scsi_cmd_length} bytes)", event
+              when "15"
+                message :scsi, "mode select (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+                @scsi_extra_count = @scsi_cmd_length.hex.to_i
+                @scsi_extra_data = []
+              when "d1"
+                message :scsi, "slide (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+                @scsi_extra_count = @scsi_cmd_length.hex.to_i
+                @scsi_extra_data = []
+              when "dc"
+                message :scsi, "set_gain_offset (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+                @scsi_extra_count = @scsi_cmd_length.hex.to_i
+                @scsi_extra_data = []
+              when "10", "11" # handled above
+                message :scsi, "-- extra data follows (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+                @scsi_extra_count = @scsi_cmd_length.hex.to_i
+                @scsi_extra_data = []
+              when "0c" then message :scsi, "0x0c (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+              when "0f" then message :scsi, "read_reverse (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+              when "12" then message :scsi, "inquiry (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+              when "16" then message :scsi, "reserve_unit (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event # used ?
+              when "18" then message :scsi, "copy (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+              when "1a" then message :scsi, "mode sense (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+              when "1b"
+                message :scsi, "scan : #{@scsi_cmd_length}:#{@scsi_cmd_data.inspect}", event
+              when "1d" then message :scsi, "diag (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+              when "a8" then message :scsi, "read (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+              when "d7" then message :scsi, "read_gain_offset (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+              when "dd" then message :scsi, "read_status (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
+              when "ff" then message :scsi, "0xff (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
               else
-                message "****\t\t\t SCSI #{@scsi_cmd_current} (#{@scsi_cmd_length} bytes)", event
+                message :scsi, "****\t\t\t #{@scsi_cmd_current} (#{@scsi_cmd_length}:#{@scsi_cmd_data.inspect})", event
               end
+              @scsi_cmd_data = []
               @scsi_cmd_index = -1
             end
             @scsi_cmd_index += 1
@@ -176,8 +263,8 @@ module UsbMon
             # 1 byte
             # sequence 0x04 (C1284_NINIT), 0x05 (C1284_NINIT | C1284_NSTROBE)
             case event.data
-            when "04" then message "Ctrl - init", event
-            when "05" then message "Ctrl - strobe", event
+            when "04" then message :ctrl, "init", event
+            when "05" then message :ctrl, "strobe", event
             else
               raise "Unknown IEEE1284 control value #{event.data.inspect}"
             end
@@ -199,11 +286,11 @@ module UsbMon
                 raise "Bad IEEE1284 command at pos 7: #{event.data}: #{event.raw}"
               end
               case @ieee1284_current_cmd
-              when 0 then message "Cmd - Addr", event
-              when 0x30 then message "Cmd - Reset", event
-              when 0xe0 then message "Cmd - SCSI", event
+              when 0 then message :cmd, "Addr", event
+              when 0x30 then message :cmd, "Reset", event
+              when 0xe0 then message :cmd, "SCSI", event
               else
-                message "Cmd 1284 - ??? #{@ieee1284_current_cmd.to_hex}", event
+                message :cmd, "??? #{@ieee1284_current_cmd.to_hex}", event
               end
               @ieee1284_cmd_index = -1
             else
@@ -258,7 +345,7 @@ module UsbMon
     # C Ci
     def interprete_callback_control_input event
       # status
-      message "\t= #{event.status}", event
+      message :usb, "\t= #{event.status}", event
     end
 
     # C Co
@@ -270,7 +357,7 @@ module UsbMon
 
     # C Bi
     def interprete_callback_bulk_input event
-      message "Receive #{event.data_s}", event
+      message :top, "Receive #{event.data_s}", event
     end
 
     # C Bo
