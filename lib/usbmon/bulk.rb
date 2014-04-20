eebe3114df11d18f2be566ac8fc6e450c61d9df7
Klaus Kämpf <kkaempf@suse.de>
2014-04-20 21:45:50 +0200

    Handle multiple BulkIn events

--- lib/usbmon/bulk.rb
+++ lib/usbmon/bulk.rb
@@ -1,13 +1,22 @@
 class BulkIn
   attr_reader :length, :data
-  def initialize stream, length
-    # S:Bi sending buffer size
-    event = stream.next UsbMon::Submission, "Bi"
-    bufsize = event.dlen
-    # C:Bi receiving data
-    result = stream.next UsbMon::Callback, "Bi"
-    raise "BulkIn length mismatch, expected #{length}, got #{result.dlen}" unless result.dlen == length
-    @length = length
-    @data = result.data
+  def initialize stream, maxlen
+    @length = 0
+    @data = []
+    loop do
+      # S:Bi sending buffer size
+      event = stream.next UsbMon::Submission, "Bi"
+      bufsize = event.dlen
+      # C:Bi receiving data
+      result = stream.next UsbMon::Callback, "Bi"
+      @length += result.dlen
+      @data << result.data
+      if @length > maxlen
+        raise "BulkIn length #{@length} exceeds maximum #{maxlen}"
+      elsif @length == maxlen
+        break
+      end
+      break unless stream.peek(UsbMon::Submission, "Bi")
+    end
   end
 end
