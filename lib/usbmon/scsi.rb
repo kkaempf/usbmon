05919fc9f8f7dfbe5d85419730c4492834d2c800
Klaus Kämpf <kkaempf@suse.de>
2014-04-21 08:00:38 +0200

    Honor multiple scsi result states

--- lib/usbmon/scsi.rb
+++ lib/usbmon/scsi.rb
@@ -20,37 +20,45 @@ end
 class ScsiStatus
   attr_reader :value
   def initialize stream
+    @values = []
     # 1 or 2 bytes scsi status
     for i in 1..2
       # scsi status
       status = ControlIn.new stream
       raise "Expecting scsi status: #{status}" unless status.port == 0x0084
+      @values << status.value
       next if status.value == 3 # scsi busy
       @value = status.value
       break
     end
   end
   def to_s
-    case @value
-    when 0 then "Ok"
-    when 1 then "Read"
-    when 2 then "Check Condition"
-    when 3 then "Busy"
-    when 8 then "Again"
-    when 255 then "Error"
-    else
-      "*** Status #{@value}"
+    s = ""
+    @values.each do |value|
+      s += ", " unless s.empty?
+      s += case value
+      when 0 then "Ok"
+      when 1 then "Read"
+      when 2 then "Check Condition"
+      when 3 then "Busy"
+      when 8 then "Again"
+      when 255 then "Error"
+      else
+        "*** Status #{@value}"
+      end
     end
+    s
   end
 end
 
 class Scsi
   attr_reader :cmd, :length, :status, :expected
-  def initialize cmd, status, stream
+  def initialize cmd, stream
 #    puts "Scsi.new #{cmd}"
     @cmd = cmd[0]
     @name = nil
     @length = cmd[3] * 255 + cmd[4]
+    status = ScsiStatus.new stream
     if status.value == 1
       # read, set length
       co = ControlOut.new stream
@@ -102,106 +110,105 @@ class Scsi
       cmd << co.value
     end
 #    puts "Scsi cmd #{cmd}"
-    status = ScsiStatus.new stream
     case cmd[0]
-    when 0x00 then Scsi_TestReady.new cmd, status, stream
-    when 0x03 then Scsi_RequestSense.new cmd, status, stream
-    when 0x08 then Scsi_Read.new cmd, status, stream
-    when 0x0a then Scsi_Write.new cmd, status, stream
-    when 0x0f then Scsi_GetParameters.new cmd, status, stream
-    when 0x15 then Scsi_ModeSelect.new cmd, status, stream
-    when 0x18 then Scsi_GetCCD.new cmd, status, stream
-    when 0x1b then Scsi_Scan.new cmd, status, stream
-    when 0xd1 then Scsi_Slide.new cmd, status, stream
-    when 0xd7 then Scsi_ReadGainOffset.new cmd, status, stream
-    when 0xdc then Scsi_WriteGainOffset.new cmd, status, stream
-    when 0xdd then Scsi_ReadStatus.new cmd, status, stream
+    when 0x00 then Scsi_TestReady.new cmd, stream
+    when 0x03 then Scsi_RequestSense.new cmd, stream
+    when 0x08 then Scsi_Read.new cmd, stream
+    when 0x0a then Scsi_Write.new cmd, stream
+    when 0x0f then Scsi_GetParameters.new cmd, stream
+    when 0x15 then Scsi_ModeSelect.new cmd, stream
+    when 0x18 then Scsi_GetCCD.new cmd, stream
+    when 0x1b then Scsi_Scan.new cmd, stream
+    when 0xd1 then Scsi_Slide.new cmd, stream
+    when 0xd7 then Scsi_ReadGainOffset.new cmd, stream
+    when 0xdc then Scsi_WriteGainOffset.new cmd, stream
+    when 0xdd then Scsi_ReadStatus.new cmd, stream
     else
-      Scsi.new cmd, status, stream
+      Scsi.new cmd, stream
     end
   end
 end
 
 class Scsi_TestReady < Scsi
-  def initialize cmd, status, stream
-    super cmd, status, stream
+  def initialize cmd, stream
+    super cmd, stream
     @name = "TestReady"
   end    
 end
 
 class Scsi_RequestSense < Scsi
-  def initialize cmd, status, stream
-    super cmd, status, stream
+  def initialize cmd, stream
+    super cmd, stream
     @name = "RequestSense"
   end
 end
 
 class Scsi_Read < Scsi
-  def initialize cmd, status, stream
-    super cmd, status, stream
+  def initialize cmd, stream
+    super cmd, stream
     @name = "Read"
   end
 end
 
 class Scsi_Write < Scsi
-  def initialize cmd, status, stream
-    super cmd, status, stream
+  def initialize cmd, stream
+    super cmd, stream
     @name = "Write"
   end
 end
 
 class Scsi_GetParameters < Scsi
-  def initialize cmd, status, stream
-    super cmd, status, stream
+  def initialize cmd, stream
+    super cmd, stream
     @name = "GetParameters"
   end
 end
 
 class Scsi_ModeSelect < Scsi
-  def initialize cmd, status, stream
-    super cmd, status, stream
+  def initialize cmd, stream
+    super cmd, stream
     @name = "ModeSelect"
   end
 end
 
 class Scsi_GetCCD < Scsi
-  def initialize cmd, status, stream
-    super cmd, status, stream
+  def initialize cmd, stream
+    super cmd, stream
     @name = "GetCCD"
   end
 end
 
 class Scsi_Scan < Scsi
-  def initialize cmd, status, stream
-    super cmd, status, stream
+  def initialize cmd, stream
+    super cmd, stream
     @name = "Scan"
   end
 end
 
 class Scsi_Slide < Scsi
-  def initialize cmd, status, stream
-    super cmd, status, stream
+  def initialize cmd, stream
+    super cmd, stream
     @name = "Slide"
   end
 end
 
 class Scsi_ReadGainOffset < Scsi
-  def initialize cmd, status, stream
-    super cmd, status, stream
+  def initialize cmd, stream
+    super cmd, stream
     @name = "ReadGainOffset"
   end
 end
 
 class Scsi_WriteGainOffset < Scsi
-  def initialize cmd, status, stream
-    super cmd, status, stream
+  def initialize cmd, stream
+    super cmd, stream
     @name = "WriteGainOffset"
   end
 end
 
 class Scsi_ReadStatus < Scsi
-  def initialize cmd, status, stream
-    super cmd, status, stream
+  def initialize cmd, stream
+    super cmd, stream
     @name = "ReadStatus"
   end
   def to_s
